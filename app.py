e30fe74a8c7923962ed5210b46408f43fa70fa57
Akarawin <117901319+Akrwin@users.noreply.github.com>
2024-04-04 20:14:25 +0700

    Update app.py
    Update 1.0.1

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import streamlit as st 
+from constant import *
 
-st.header("Hello World 👏")
+st.set_page_config(page_title='Akkarawin portfolio' ,layout="wide",page_icon='👨‍🔬')
 
-st.write("This is Akkarawin apps")
+st.header('Akkarawin Saiprapakorn👏')
 
+st.write('This is my portfolio')
+st.write(info['brief'])
+
+st.subheader('Education📖')
+
+st.subheader('Experience📖')
