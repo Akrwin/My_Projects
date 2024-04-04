132b4b8fb792a5da05c651316baef470f3d20f19
Akarawin <117901319+Akrwin@users.noreply.github.com>
2024-04-04 20:15:43 +0700

    Add files via upload

---
+++ constant.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import graphviz as graphviz
+
+info = {'name':'Akkarawin Saiprapakorn',
+        'brief':'..info something',
+        'mobile':'090-768-3100',
+        'email':'ak4rawin@gmail.com',
+        'github':'https://github.com/Akrwin',
+        'skills':['Tableau','SQL','Snowflake','Streamlit','Data Analysis']}
