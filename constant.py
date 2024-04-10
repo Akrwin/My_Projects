ae1da92babf0b66701dd4d3f2c4da3de0ab2717f
Akarawin <117901319+Akrwin@users.noreply.github.com>
2024-04-10 16:22:59 +0700

    Update constant.py

--- constant.py
+++ constant.py
@@ -6,7 +6,7 @@ info = {
         'mobile':'090-768-3100',
         'email':'ak4rawin@gmail.com',
         'github':'https://github.com/Akrwin',
-        'skills':['Tableau','SQL','Snowflake','Streamlit','Data Analysis','Python'],
+        'skills':['R','Tableau','SQL','Python','Google Sheets/Excel','Snowflake','Streamlit'],
         'linkedin':'<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script><div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="ak4rawin" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://th.linkedin.com/in/ak4rawin?trk=profile-badge"></a></div>'
         }
 
