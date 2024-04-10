59c0dd99d6dcbb1af669de091228e0c64a43dc3d
Akarawin <117901319+Akrwin@users.noreply.github.com>
2024-04-10 16:48:03 +0700

    Update constant.py

--- constant.py
+++ constant.py
@@ -1,16 +1,26 @@
 import pandas as pd
 
 info = {
-        'name':'Akkarawin Saiprapakorn',
-        'brief':'..info something',
-        'mobile':'090-768-3100',
-        'email':'ak4rawin@gmail.com',
-        'github':'https://github.com/Akrwin',
-        'skills':['R','Tableau','SQL','Python','Google Sheets/Excel','Snowflake','Streamlit'],
-        'linkedin':'<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script><div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="ak4rawin" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://th.linkedin.com/in/ak4rawin?trk=profile-badge"></a></div>'
+    'name':'Akkarawin Saiprapakorn',
+    'brief':'..info something',
+    'mobile':'090-768-3100',
+    'email':'ak4rawin@gmail.com',
+    'github':'https://github.com/Akrwin',
+    'skills':['R','Tableau','SQL','Python','Google Sheets/Excel','Snowflake','Streamlit'],
+    'linkedin':'<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script><div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="ak4rawin" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://th.linkedin.com/in/ak4rawin?trk=profile-badge"></a></div>'
         }
 
-educations = {'Institute':'Kasetsart University',
-              'Qualification':'Bachelor of Science',
-              'Major':'Statistic',
-              'Year':'2018-2022'}
+educations = {
+    'Institute':'Kasetsart University',
+    'Qualification':'Bachelor of Science',
+    'Major':'Statistic',
+    'Year':'2018-2022'
+        }
+
+work_exp = {
+    'corp':['QOL Enterprises','LEARN Corporation (Ignite by Ondemand)'],
+    'year':['May 2022 - Feb 2023','Mar 2023 - Present'],
+    'des1':['''Analyze daily/weekly/monthly reports
+summarize the cost and profit of the sales
+Create a dashboard and present solution to increase profits for the company.''' , '''just wait''']
+}
