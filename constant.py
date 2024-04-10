10aa11352e1633bc8fa2faa3564b2e33106cdac1
Akarawin <117901319+Akrwin@users.noreply.github.com>
2024-04-10 16:01:37 +0700

    Update constant.py

--- constant.py
+++ constant.py
@@ -1,11 +1,14 @@
 import pandas as pd
 
-info = {'name':'Akkarawin Saiprapakorn',
+info = {
+        'name':'Akkarawin Saiprapakorn',
         'brief':'..info something',
         'mobile':'090-768-3100',
         'email':'ak4rawin@gmail.com',
         'github':'https://github.com/Akrwin',
-        'skills':['Tableau','SQL','Snowflake','Streamlit','Data Analysis','Python']}
+        'skills':['Tableau','SQL','Snowflake','Streamlit','Data Analysis','Python'],
+        'linkedin':'<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script><div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="ak4rawin" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://th.linkedin.com/in/ak4rawin?trk=profile-badge">Akkarawin Saiprapakorn</a></div>'
+        }
 
 educations = {'Institute':'Kasetsart University',
               'Qualification':'Bachelor of Science',
