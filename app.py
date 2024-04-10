6d69745a7218092b0f6ddbc5c55807a0c5ab84ce
Akarawin <117901319+Akrwin@users.noreply.github.com>
2024-04-10 15:08:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,36 +9,27 @@ st.write('This is my portfolio')
 st.write(info['brief'])
 
 # Skill 
-st.subheader('Skills & Tools')
+st.subheader('Technical Skills')
 
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.button(info['skills'][0])
     st.button(info['skills'][1])
+    st.button(info['skills'][2])
     
 with col2:
-    st.button(info['skills'][2])
     st.button(info['skills'][3])
+    st.button(info['skills'][4])
 
 with col3:
-    st.button(info['skills'][4])
     st.button(info['skills'][5])
+    st.button(info['skills'][0])
 
 # Education
 st.subheader('Education📖')
-df = pd.DataFrame(data, columns=['Institute','Qualification ','Major','Year'])
-hide_table_row_index = """
-            <style>
-            thead tr th:first-child {display:none}
-            tbody th {display:none}
-            </style>
-            """
-# Inject CSS with Markdown
-st.markdown(hide_table_row_index, unsafe_allow_html=True)
-st.table(df)
+st.markdown('<h5><u>'+educations['Institute']+'</h5>',unsafe_allow_html=True)
+st.caption(educations['Qualification']+educations['Major'])
+st.caption(educations['Year'])
 
 # Experience
 st.subheader('Experience')
-
-
