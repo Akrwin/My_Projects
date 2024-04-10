6cc365b3d21e45bd1e5b20b14223e46d2a9d49bc
Akarawin <117901319+Akrwin@users.noreply.github.com>
2024-04-10 16:07:32 +0700

    Update constant.py

--- constant.py
+++ constant.py
@@ -7,7 +7,7 @@ info = {
         'email':'ak4rawin@gmail.com',
         'github':'https://github.com/Akrwin',
         'skills':['Tableau','SQL','Snowflake','Streamlit','Data Analysis','Python'],
-        'linkedin':'<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script><div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="ak4rawin" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://th.linkedin.com/in/ak4rawin?trk=profile-badge">Akkarawin Saiprapakorn</a></div>'
+        'linkedin':'<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script><div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="ak4rawin" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://th.linkedin.com/in/ak4rawin?trk=profile-badge"></a></div>'
         }
 
 educations = {'Institute':'Kasetsart University',
