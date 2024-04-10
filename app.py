b789b62bda50770b9cf325f5ccf4231fd3760004
Akarawin <117901319+Akrwin@users.noreply.github.com>
2024-04-10 16:58:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -39,10 +39,13 @@ st.caption(educations['Year'])
 
 # Experience
 st.title('Work Experience 🗂️')
-st.markdown('<h5>'+work_exp['corp'][0]+'</h5>',unsafe_allow_html=True)
-st.caption(work_exp['year'][0])
-st.markdown('<ul>'+'HUHHHHHH'+'</ul>',unsafe_allow_html=True)
 
-st.markdown('<h5>'+work_exp['corp'][1]+'</h5>',unsafe_allow_html=True)
-st.caption(work_exp['year'][1])
-st.write(work_exp['des'][1])
+st.markdown('<h5>'+work_exp['corp'][0]+'</h5>',unsafe_allow_html=True)
+# st.caption(work_exp['year'][0])
+# st.markdown('<ul>'+'HUHHHHHH'+'</ul>',unsafe_allow_html=True)
+
+# st.markdown('<h5>'+work_exp['corp'][1]+'</h5>',unsafe_allow_html=True)
+# st.caption(work_exp['year'][1])
+# st.write(work_exp['des'][1])
+achievement_list = ''.join(['<li>'+item+'</li>' for item in work_exp['des'][0]])
+st.markdown('<ul>'+achievement_list+'</ul>',unsafe_allow_html=True)
