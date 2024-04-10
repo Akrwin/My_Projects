e6d84c9306caf5d8e81e7b48704c83b526f6561c
Akarawin <117901319+Akrwin@users.noreply.github.com>
2024-04-10 14:24:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,23 @@ st.header('Akkarawin Saiprapakorn👏')
 st.write('This is my portfolio')
 st.write(info['brief'])
 
+st.subheader('Skills & Tools')
+
+def skill_tab():
+    rows,cols = len(info['skills'])//skill_col_size,skill_col_size
+    skills = iter(info['skills'])
+    if len(info['skills'])%skill_col_size!=0:
+        rows+=1
+    for x in range(rows):
+        columns = st.columns(skill_col_size)
+        for index_ in range(skill_col_size):
+            try:
+                columns[index_].button(next(skills))
+            except:
+                break
+with st.spinner(text="Loading section..."):
+    skill_tab()
+
 st.subheader('Education📖')
 
 st.subheader('Experience📖')
