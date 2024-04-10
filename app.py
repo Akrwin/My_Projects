d286e6fc8a4d3f993ab4126c1eb8235d77933cc4
Akarawin <117901319+Akrwin@users.noreply.github.com>
2024-04-10 15:23:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ with col3:
 
 # Education
 st.subheader('Education📖')
-st.markdown('    <h5>'+educations['Institute']+'</h5>',unsafe_allow_html=True)
+st.markdown('<h5>:red['+educations['Institute']+']:</h5>',unsafe_allow_html=True)
 st.caption('    '+educations['Qualification']+educations['Major'])
 st.caption('    '+educations['Year'])
 
