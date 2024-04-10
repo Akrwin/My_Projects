bc228ed66fa5a924f2163ea1bc3e22b75460df99
Akarawin <117901319+Akrwin@users.noreply.github.com>
2024-04-10 14:47:21 +0700

    Update constant.py
    Change skills

--- constant.py
+++ constant.py
@@ -5,7 +5,7 @@ info = {'name':'Akkarawin Saiprapakorn',
         'mobile':'090-768-3100',
         'email':'ak4rawin@gmail.com',
         'github':'https://github.com/Akrwin',
-        'skills':['Tableau','SQL','Snowflake','Streamlit','Data Analysis','Python']}
+        'skills':['R','Python','SQL','Snowflake','PowerBI','Excel/GGSheet']}
 
 df_col = ['Institute','Qualification ','Major','Year']
 data = [['Kasetsart University','Bachelor of Science','Statistic','2018-2022']]
